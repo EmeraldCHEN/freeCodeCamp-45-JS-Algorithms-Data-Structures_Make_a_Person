0b8ae677c71faa1b541ac13dc787dbf06a196a86
Emerald Chen <ahyuancy@gmail.com>
2019-01-06 11:30:12 +1300

    Create .js

---
+++ .js
@@ -0,0 +1,33 @@
+let Person = function(firstAndLast) {
+  // Copy the full name that was passed as a parameter
+    let fullName = firstAndLast;
+ //Create six methods needed and return what is asked for
+    this.getFirstName = function(){
+        return fullName.split(' ')[0];
+    }
+    this.getLastName = function(){
+        return fullName.split(' ')[1];
+    }
+
+    this.getFullName = function(){
+        return fullName;
+    }
+    this.setFirstName = function(name){
+        fullName = name + " " + fullName.split(' ')[1];
+    }
+    this.setLastName = function(name){
+        fullName = fullName.split(' ')[0] + " " + name;
+    }
+    this.setFullName = function(name){
+        fullName = name;
+    }
+}
+var bob = new Person('Bob Ross');
+bob.getFullName();
+
+
+
+
+// Retrieved from https://guide.freecodecamp.org/certifications/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/make-a-person/
+
+
